f7471acb1f9d881f3190e0dd9981c58b923b6bec
Richard Hull <rm_hull@yahoo.co.uk>
2013-06-24 23:32:22 +0100

    Initial project files

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject sound-flour "0.0.1"
+  :description "an experiment in collaborative broadcast streaming computer-generated music"
+  :url "https://github.com/rm-hull/sound-flour"
+  :license {:name "Creative Commons 3.0"
+            :url "http://creativecommons.org/licenses/by/3.0/legalcode"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [instaparse "1.1.0"]]
+  :source-path "src"
+  :min-lein-version "2.2.0"
+  :description "TODO" 
+  :global-vars { *warn-on-reflection* true }) 
