417df777485c7b97052defc22f138235a5b8d898
Richard Hull <rm_hull@yahoo.co.uk>
2016-04-30 14:31:51 +0100

    More rationalisation

--- src/sound_flour/handler.clj
+++ src/sound_flour/handler.clj
@@ -7,79 +7,31 @@
     [metrics.ring.expose :refer [expose-metrics-as-json]]
     [metrics.ring.instrument :refer [instrument]]
     [infix.macros :refer [infix from-string]]
-    [sound-flour.encoder :refer [audio-stream clip scale]]
+    [sound-flour.encoder :refer [audio-stream]]
+    [sound-flour.byte-converters :refer [ubyte ushort]]
     [sound-flour.oscillators :refer [sine-wave]])
   (:import
     [sound-flour FunctionInputStream]))
 
-(def sin
-  (comp
-    scale
-    clip
-    (sine-wave 440 0.5))) ; 440 Hz = Middle C
-
-(defn a [t]
-  (infix (t * (t >> 5 | t >> 8)) >> (t >> 16)))
-
-(defn b [t]
-  (infix (t * ((t >> 12 | t >> 8) & 63 & t >> 4))))
-
-(defn c [t]
-  (infix  (t >> 6 | t | t >> (t >> 16)) * 10 + ((t >> 11) & 7)))
-
-(defn d [t]
-  (infix (t % (t / (t >> 9 | t >> 13))) ))
-
-
-(defn upscale-8-bit [v]
-  (infix 0xff * (0xff & v)))
+(defn clip [^double d]
+  (Math/max -1.0 (Math/min d 1.0)))
 
 (defroutes app-routes
   (GET "/sine-wave" []
     (->
-      (response (audio-stream 8000 16 (FunctionInputStream. sin)))
+      (comp ushort (partial * 0x7fff) clip (sine-wave 440 0.5))  ; 440 Hz = Middle C
+      (FunctionInputStream. 16)
+      (audio-stream 8000 16)
+      (response)
       (content-type "audio/x-wav")))
 
-  (GET "/8-bit-trip/:eqn" [eqn]
+  (GET "/8-bit-trip/:expr" [expr]
    (->
-     (comp upscale-8-bit (from-string [t] eqn))
-     (FunctionInputStream.)
-     (audio-stream 8000 16)
+     (comp ubyte (partial bit-and 0xFF) (from-string [t] expr))
+     (FunctionInputStream. 8)
+     (audio-stream 8000 8)
      (response)
-     (content-type "audio/x-wav")))
-
-  (GET "/a" []
-    (->
-      (comp upscale-8-bit a)
-      (FunctionInputStream.)
-      (audio-stream 8000 16)
-      (response)
-      (content-type "audio/x-wav")))
-
-  (GET "/b" []
-    (->
-      (comp upscale-8-bit b)
-      (FunctionInputStream.)
-      (audio-stream 8000 16)
-      (response)
-      (content-type "audio/x-wav")))
-
-  (GET "/c" []
-    (->
-      (comp upscale-8-bit c)
-      (FunctionInputStream.)
-      (audio-stream 8000 16)
-      (response)
-      (content-type "audio/x-wav")))
-
-  (GET "/d" []
-    (->
-      (comp upscale-8-bit d)
-      (FunctionInputStream.)
-      (audio-stream 8000 16)
-      (response)
-      (content-type "audio/x-wav"))))
-
+     (content-type "audio/x-wav"))))
 
 (def app
   (->
