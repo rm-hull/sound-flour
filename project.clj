6ba583c2d80a65489cac554fca611d0f2f0f1d6f
Richard Hull <rm_hull@yahoo.co.uk>
2016-06-15 19:58:29 +0100

    Update dips

--- project.clj
+++ project.clj
@@ -7,27 +7,26 @@
   :dependencies [
     [org.clojure/clojure "1.8.0"]
     [compojure "1.5.0"]
-    [ring "1.4.0"]
-    [metrics-clojure-ring "2.6.1"]
+    [ring "1.5.0"]
+    [metrics-clojure-ring "2.7.0"]
     [ring-logger-timbre "0.7.5"]
-    [com.taoensso/timbre "4.3.1"]
-    [rm-hull/infix "0.2.6"]]
+    [com.taoensso/timbre "4.4.0"]
+    [rm-hull/infix "0.2.8"]]
   :scm {:url "git@github.com:rm-hull/sound-flour.git"}
   :ring {
     :handler sound-flour.handler/app }
   :plugins [
     [lein-ring "0.9.7"]
-    [codox "0.9.4"]]
+    [lein-codox "0.9.5"]]
   :aot [sound-flour.FunctionInputStream]
   :source-paths ["src"]
   :resource-paths ["resources"]
   :jar-exclusions [#"(?:^|/).git"]
   :uberjar-exclusions [#"\.SF" #"\.RSA" #"\.DSA"]
   :codox {
-    :sources ["src"]
-    :output-dir "doc/api"
-    :src-dir-uri "http://github.com/rm-hull/sound-flour/blob/master/"
-    :src-linenum-anchor-prefix "L" }
+    :source-paths ["src"]
+    :output-path "doc/api"
+    :source-uri "http://github.com/rm-hull/sound-flour/blob/master/{filepath}#L{line}" }
   :min-lein-version "2.6.1"
   :profiles {
     :uberjar {:aot :all}
